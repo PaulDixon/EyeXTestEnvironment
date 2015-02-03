38ddecaccdd80cf77250eaa642967d1897a9e4ae
Paul Dixon <chris_paul_dixon@hotmail.com>
2015-02-03 15:00:35 +0100

    Ball Collision

---
+++ UnityEyeXTestEnvironment/Assets/RayCastCollide.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RayCastCollide : MonoBehaviour {
+	public GameObject particle;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Fire1")) {
+			Debug.Log("mouse fire");
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (collider.Raycast(ray, out hit, 100.0F))
+				Debug.Log(ray.origin.ToString() + " " + hit.point.ToString());
+			Debug.DrawLine(ray.origin, hit.point);
+			particle.transform.position = hit.point;
+			
+			
+		}
+	}
+}
--- UnityEyeXTestEnvironment/Assets/SpinOnGaze.cs
+++ UnityEyeXTestEnvironment/Assets/SpinOnGaze.cs
@@ -3,27 +3,71 @@ using System.Collections;
 
 public class SpinOnGaze : MonoBehaviour {
 
+
 	private GazeAwareComponent _gazeAware;
 	private UserPresenceComponent _userPresence;
+	private GazePointDataComponent _lastGazePoint;
+
 	
 	void Start () 
 	{
+
 		_gazeAware = GetComponent<GazeAwareComponent>();
 		_userPresence = GetComponent<UserPresenceComponent>();
+		_lastGazePoint = GetComponent<GazePointDataComponent>();
 
-	}
 	
+	}/*
+	
+	public static float DistanceToLine(Ray ray, Vector3 point)
+	{
+		return Vector3.Cross(ray.direction, point - ray.origin).magnitude;
+	}
+	*/
+
+
 	void Update () 
 	{
-		if (!_userPresence.IsUserPresent)
-		{
-			Debug.Log("WHERE ARE YOU!!!");
-		}
+
+
+
+			// Get the last gaze point.
+
+			if (_lastGazePoint.LastGazePoint.IsValid) {
+						// Convert the fixation data to screen space.
+						Vector2 screenSpace = _lastGazePoint.LastGazePoint.Screen;
+						//Debug.Log (screenSpace.ToString());
+		
+
+				} else {
+			Debug.Log("Not Valid gaze point");
+				}
+
+
+		if (!_userPresence.IsUserPresent) {
+					//	Debug.Log ("WHERE ARE YOU!!!");
+						renderer.enabled = false;
+
+				}
+		else {
+			renderer.enabled = true;
+				}
+
 
 // */
-		if (_gazeAware.HasGaze) 
-		{
-			transform.Rotate(Vector3.up);
+		if (_gazeAware.HasGaze) {
+			transform.Rotate (Vector3.up);
+			renderer.material.color = Color.red;
+		} else {
+			renderer.material.color = Color.white;		
 		}
+		/*
+		if (Physics.Raycast (ray, hit, 100.0)) {
+			//hit.distance is the distance to the first collider.
+			distanceToGround = hit.distance;
+			//This draws a line, that shows the ray, in the editor.
+			Debug.DrawLine (ray.origin, hit.point);
+		}
+		*/
 	}
 }
